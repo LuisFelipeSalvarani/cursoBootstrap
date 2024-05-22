1011f8d464d38bfac7b1f94414c94b14c796e195
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-05-22 17:50:18 -0300

    Tooltip

--- componentes/exemplo-2/js/scripts.js
+++ componentes/exemplo-2/js/scripts.js
@@ -2,3 +2,10 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl)
 })
+
+// Tooptip
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'),)
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
